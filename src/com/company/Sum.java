23063126cc18751c04fbff054de97ff876097883
ajskfdl <chaen_119@naver.com>
2021-08-22 20:55:22 +0900

    init project

---
+++ src/com/company/Sum.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Sum {
+
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        int b = scanner.nextInt();
+
+        int c = a + b;
+
+        System.out.println( c );
+    }
+}
+
+
